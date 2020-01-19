790ed1f5974d05b648fca0ae18600f22f5c7fca2
sunshixiong <1285913468@qq.com>
2020-01-19 11:08:17 +0800

    修改页面

--- src/main/java/com/imedcare/project/addProject/DemoViewController.java
+++ src/main/java/com/imedcare/project/addProject/DemoViewController.java
@@ -1,5 +1,6 @@
 package com.imedcare.project.addProject;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,38 +15,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/demoView")
 public class DemoViewController {
 
+  @RequiresPermissions("cqbj:bdj:view")
   @GetMapping("chanqian")
-  public String chanQian(){
-    return "/demoview/chanqian/bdj";
+  public String chanQian() {
+    return "demoview/chanqian/bdj";
   }
 
   @GetMapping("chanqian/add")
-  public String chanQianAdd(){
-    return "/demoview/chanqian/add";
+  public String chanQianAdd() {
+    return "demoview/chanqian/add";
   }
 
   @GetMapping("zzjl")
-  public String zzjl(){
-    return "/demoview/zzjl/zzjl";
+  @RequiresPermissions("cqbj:zzjl:view")
+  public String zzjl() {
+    return "demoview/zzjl/zzjl";
   }
+
+  @RequiresPermissions("cqbj:zzjl:view")
   @GetMapping("yangxing")
-  public String yangxing(){
-    return "/demoview/yangxing/zzjl";
+  public String yangxing() {
+    return "demoview/yangxing/zzjl";
   }
+
+  @RequiresPermissions("cqbj:zzjl:view")
   @GetMapping("jianche")
-  public String jianche(){
-    return "/demoview/jianche/zzjl";
+  public String jianche() {
+    return "demoview/jianche/zzjl";
   }
+
+  @RequiresPermissions("cqbj:zzjl:view")
   @GetMapping("shenghe")
-  public String shenghe(){
-    return "/demoview/shenghe/zzjl";
+  public String shenghe() {
+    return "demoview/shenghe/zzjl";
   }
+
+  @RequiresPermissions("cqbj:zzjl:view")
   @GetMapping("pingsheng")
-  public String pingsheng(){
-    return "/demoview/pingsheng/zzjl";
+  public String pingsheng() {
+    return "demoview/pingsheng/zzjl";
   }
+
+  @RequiresPermissions("cqbj:zzjl:view")
   @GetMapping("yujing")
-  public String yujing(){
-    return "/demoview/yujing/zzjl";
+  public String yujing() {
+    return "demoview/yujing/zzjl";
   }
 }
